dcfb0c79ae9d18cbefc71907558d35121d379f1c
Tim Davis <contact@timdav.is>
2023-10-26 17:07:15 -0700

    simplify to just plain sql setup

--- dumbo.go
+++ dumbo.go
@@ -5,9 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"reflect"
 	"runtime"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -18,213 +16,49 @@ type DB interface {
 	Query(string, ...any) (*sql.Rows, error)
 }
 
-type Schema struct {
-	Table   string
-	Columns map[string]string
-}
-
-type Dumbo struct {
-	schemas map[string]Schema
-}
-
-func New(schemas ...Schema) Dumbo {
-	s := make(map[string]Schema, len(schemas))
-	for _, schema := range schemas {
-		s[schema.Table] = schema
-	}
-	return Dumbo{
-		schemas: s,
-	}
-}
-
-func (d Dumbo) Seed(t *testing.T, db DB, table string, record any) {
-	t.Helper()
-
-	_, err := db.Exec(fmt.Sprintf(`truncate table %v restart identity cascade`, identifier(table)))
-	require.NoError(t, err, fmt.Sprintf("truncating table %v", identifier(table)))
-
-	d.Insert(t, db, table, record)
-}
-
-func (d Dumbo) Insert(t *testing.T, db DB, table string, record any) {
-	t.Helper()
-
-	records := many(record)
-	schema := d.schemas[table]
-
-	columns := make([]string, 0, len(schema.Columns))
-	sFields := make([]string, 0, len(schema.Columns))
-	for c, f := range schema.Columns {
-		columns = append(columns, identifier(c))
-		sFields = append(sFields, f)
-	}
-
-	tuples := make([]string, 0, len(records))
-	inputs := make([]any, 0, len(records)*len(columns))
-	param := 1
-
-	for _, p := range records {
-		input := make([]any, 0, len(sFields))
-		output := make([]any, 0, len(sFields))
-		tuple := make([]string, 0, len(sFields))
-		sValue := reflect.Indirect(reflect.ValueOf(p))
-		for _, f := range sFields {
-			var value any = sValue.FieldByName(f).Interface()
-			switch value.(type) {
-			case sql.NullBool:
-				if !value.(sql.NullBool).Valid {
-					tuple = append(tuple, "default")
-				} else {
-					input = append(input, value)
-					tuple = append(tuple, fmt.Sprintf("$%d", param))
-					param++
-				}
-			case sql.NullByte:
-				if !value.(sql.NullByte).Valid {
-					tuple = append(tuple, "default")
-				} else {
-					input = append(input, value)
-					tuple = append(tuple, fmt.Sprintf("$%d", param))
-					param++
-				}
-			case sql.NullFloat64:
-				if !value.(sql.NullFloat64).Valid {
-					tuple = append(tuple, "default")
-				} else {
-					input = append(input, value)
-					tuple = append(tuple, fmt.Sprintf("$%d", param))
-					param++
-				}
-			case sql.NullInt16:
-				if !value.(sql.NullInt16).Valid {
-					tuple = append(tuple, "default")
-				} else {
-					input = append(input, value)
-					tuple = append(tuple, fmt.Sprintf("$%d", param))
-					param++
-				}
-			case sql.NullInt32:
-				if !value.(sql.NullInt32).Valid {
-					tuple = append(tuple, "default")
-				} else {
-					input = append(input, value)
-					tuple = append(tuple, fmt.Sprintf("$%d", param))
-					param++
-				}
-			case sql.NullInt64:
-				if !value.(sql.NullInt64).Valid {
-					tuple = append(tuple, "default")
-				} else {
-					input = append(input, value)
-					tuple = append(tuple, fmt.Sprintf("$%d", param))
-					param++
-				}
-			case sql.NullString:
-				if !value.(sql.NullString).Valid {
-					tuple = append(tuple, "default")
-				} else {
-					input = append(input, value)
-					tuple = append(tuple, fmt.Sprintf("$%d", param))
-					param++
-				}
-			case sql.NullTime:
-				if !value.(sql.NullTime).Valid {
-					tuple = append(tuple, "default")
-				} else {
-					input = append(input, value)
-					tuple = append(tuple, fmt.Sprintf("$%d", param))
-					param++
-				}
-			default:
-				input = append(input, value)
-				tuple = append(tuple, fmt.Sprintf("$%d", param))
-				param++
-			}
-			output = append(output, &value)
-		}
-		inputs = append(inputs, input...)
-		tuples = append(tuples, fmt.Sprintf("(%v)", strings.Join(tuple, ", ")))
-	}
-
-	query := fmt.Sprintf(`
-		insert into %v (%v)
-		values %v
-		returning %v
-	`, identifier(table), strings.Join(columns, ", "), strings.Join(tuples, ", "), strings.Join(columns, ", "))
-
-	rows, err := db.Query(query, inputs...)
-	require.NoError(t, err, fmt.Sprintf("inserting row(s) into table %v with query: \n%v\n", identifier(table), query))
-
-	i := 0
-	for rows.Next() {
-		outputs := make([]any, 0, len(columns))
-		sValue := reflect.ValueOf(records[i]).Elem()
-		for _, sField := range sFields {
-			sField := sValue.FieldByName(sField).Addr().Interface()
-			outputs = append(outputs, sField)
-		}
-		require.NoError(t, rows.Scan(outputs...), "scanning row returned from query")
-		i++
-	}
-
-	require.NoError(t, rows.Err(), "iterating rows returned from query")
-}
-
-func many(record any) []any {
-	value := reflect.ValueOf(record)
-	if reflect.Indirect(value).Kind() != reflect.Slice {
-		return []any{value.Interface()}
-	}
-	slice := reflect.Indirect(value)
-	records := make([]any, 0, slice.Len())
-	for i := 0; i < slice.Len(); i++ {
-		records = append(records, slice.Index(i).Interface())
-	}
-	return records
-}
-
-func identifier(name string) string {
-	parts := strings.Split(name, ".")
-	for i, p := range parts {
-		if !strings.HasPrefix(p, "\"") && !strings.HasSuffix(p, "\"") {
-			parts[i] = "\"" + p + "\""
-		}
-	}
-	return strings.Join(parts, ".")
-}
-
 func RequireDB(t *testing.T) *sql.DB {
 	t.Helper()
+
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	t.Cleanup(func() { require.NoError(t, db.Close()) })
 	require.NoError(t, err)
+
+	t.Cleanup(func() { require.NoError(t, db.Close()) })
+
 	return db
 }
 
 func RequireBegin(t *testing.T, db *sql.DB) *sql.Tx {
 	t.Helper()
+
 	tx, err := db.Begin()
-	t.Cleanup(func() { require.NoError(t, tx.Rollback()) })
 	require.NoError(t, err)
+
+	t.Cleanup(func() { require.NoError(t, tx.Rollback()) })
+
 	return tx
 }
 
 func RequireSavepoint(t *testing.T, tx *sql.Tx) *sql.Tx {
 	t.Helper()
+
 	_, err := tx.Exec(fmt.Sprintf("savepoint %q", t.Name()))
+	require.NoError(t, err)
+
 	t.Cleanup(func() {
 		_, err := tx.Exec(fmt.Sprintf("rollback to savepoint %q", t.Name()))
 		require.NoError(t, err)
 	})
-	require.NoError(t, err)
+
 	return tx
 }
 
-func RequireSeed(t *testing.T, db DB, path string) {
+func RequireScript(t *testing.T, db DB, scriptPath string) {
 	t.Helper()
 
 	_, caller, _, _ := runtime.Caller(1)
-	script, pathErr := filepath.Abs(filepath.Join(filepath.Dir(caller), path))
+	relativePath := filepath.Join(filepath.Dir(caller), scriptPath)
+
+	script, pathErr := filepath.Abs(relativePath)
 	require.NoError(t, pathErr)
 
 	sql, readErr := os.ReadFile(script)
@@ -232,4 +66,11 @@ func RequireSeed(t *testing.T, db DB, path string) {
 
 	_, execErr := db.Exec(string(sql))
 	require.NoError(t, execErr)
+}
+
+func RequireExec(t *testing.T, db DB, query string) {
+	t.Helper()
+
+	_, err := db.Exec(query)
+	require.NoError(t, err)
 }
--- dumbo_test.go
+++ dumbo_test.go
@@ -16,83 +16,26 @@ type User struct {
 	IsSilly  sql.NullBool
 }
 
-func TestSeedingRecords(t *testing.T) {
-
-	d := New(Schema{
-		Table: "user",
-		Columns: map[string]string{
-			"id":       "ID",
-			"username": "Username",
-			"nickname": "Nickname",
-			"age":      "Age",
-			"is_silly": "IsSilly",
-		},
-	})
-
-	db := RequireDB(t)
-
-	t.Run("seeding a single record", func(t *testing.T) {
-		tx := RequireBegin(t, db)
-
-		user := &User{
-			Username: sql.NullString{String: "gopher", Valid: true},
-			Age:      sql.NullInt64{Int64: 24, Valid: true},
-			Nickname: "f",
-		}
-
-		d.Seed(t, tx, "user", user)
-
-		assert.Equal(t, int64(1), user.ID.Int64)
-		assert.Equal(t, "gopher", user.Username.String)
-		assert.Equal(t, int64(24), user.Age.Int64)
-		assert.False(t, user.IsSilly.Valid)
-		assert.Equal(t, "f", user.Nickname)
-	})
-
-	t.Run("seeding multiple records", func(t *testing.T) {
-		tx := RequireBegin(t, db)
-
-		users := []*User{
-			{
-				Username: sql.NullString{String: "gopher", Valid: true},
-				Age:      sql.NullInt64{Int64: 24, Valid: true},
-			},
-			{
-				Username: sql.NullString{String: "rustacean", Valid: true},
-				Age:      sql.NullInt64{Int64: 42, Valid: true},
-				IsSilly:  sql.NullBool{Bool: true, Valid: true},
-			},
-		}
-
-		d.Seed(t, tx, "user", users)
-
-		gopher, rustacean := users[0], users[1]
-
-		assert.Equal(t, int64(1), gopher.ID.Int64)
-		assert.Equal(t, "gopher", gopher.Username.String)
-		assert.Equal(t, "", gopher.Nickname)
-		assert.Equal(t, int64(24), gopher.Age.Int64)
-		assert.False(t, gopher.IsSilly.Valid)
-
-		assert.Equal(t, int64(2), rustacean.ID.Int64)
-		assert.Equal(t, "rustacean", rustacean.Username.String)
-		assert.Equal(t, int64(42), rustacean.Age.Int64)
-		assert.Equal(t, "", rustacean.Nickname)
-		assert.True(t, rustacean.IsSilly.Bool)
-		assert.True(t, rustacean.IsSilly.Valid)
-	})
-}
-
 func TestSeedingScripts(t *testing.T) {
 
 	db := RequireDB(t)
 
 	t.Run("seeding from a script file", func(t *testing.T) {
 		tx := RequireBegin(t, db)
+		RequireExec(t, tx, `--sql
+			truncate table "user" restart identity cascade;
+			insert into "user" (
+				"username",
+				"nickname",
+				"age",
+				"is_silly"
+			)
+			values
+				('gopher', 'nibbles', 24, default),
+				('rustacean', 'crab', 42, true);
+		`)
 
-		RequireSeed(t, tx, "./scripts/insert_users.sql")
-
-		query := `
+		query := `--sql
 			select
 				"id",
 				"username",
